89023dc0bd840d31e3cad63212eab9d8b405f161
noornisar <noornisar96@gmail.com>
2026-08-27 19:03:36 +0500

    Update Vercel configuration to use WhiteNoise without build_files.sh

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 
 # allow us to work locally 
